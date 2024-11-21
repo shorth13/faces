0817fcf8b13247c866ebc4c16ab6c2ebe75db1cb
Marek Rychlik <rychlik@arizona.edu>
2024-11-20 20:05:50 -0700

    *** empty log message ***

--- main.m
+++ main.m
@@ -1,2 +1,7 @@
+targetSize=[128,128];
 location = fullfile('lfw');
+
 imds = imageDatastore(location,'IncludeSubfolders',true,'LabelSource','foldernames');
+[w,h,c]=size(imds.readimage(1));
+ds = transform(imds,@(I)imresize(im2gray(I),targetSize),'IncludeInfo',false);
+montage(preview(ds));
