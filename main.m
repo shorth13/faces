3d40954808012e0e15f06811397f018ae554aa14
Marek Rychlik <rychlik@arizona.edu>
2024-11-20 20:48:32 -0700

    *** empty log message ***

--- main.m
+++ main.m
@@ -3,5 +3,11 @@ location = fullfile('lfw');
 
 imds = imageDatastore(location,'IncludeSubfolders',true,'LabelSource','foldernames',...
                       'ReadFcn', @(filename)imresize(im2gray(imread(filename)),targetSize));
-montage(preview(imds));
+%montage(preview(imds));
 A=readall(imds);
+
+% Play faces
+for j=1:length(A)
+    imshow(A{j}),title(imds.Labels(j),'Interpreter','none');
+    pause(1);
+end
