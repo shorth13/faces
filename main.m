4070ec376e26b2acb21d466d544e81e085ff8494
Marek Rychlik <rychlik@arizona.edu>
2024-11-20 21:32:29 -0700

    *** empty log message ***

--- main.m
+++ main.m
@@ -11,3 +11,17 @@ for j=1:length(A)
     imshow(A{j}),title(imds.Labels(j),'Interpreter','none');
     pause(1);
 end
+
+B=cat(3,A{:});
+imshow(B(:,:,1))
+D=prod(targetSize);
+B=reshape(B,D,[]);
+B=single(B)./256;
+tic;
+[U,S,V]=svd(B,'econ');
+toc;
+
+for j=1:size(U,2)
+    imagesc(reshape(U(:,j),targetSize));
+    pause(1);
+end
