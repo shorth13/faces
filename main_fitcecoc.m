1fcc7b0dd7ce13a380d056ab30b26bac4d347a6e
Marek Rychlik <rychlik@arizona.edu>
2024-11-30 10:37:48 -0700

    *** empty log message ***

--- main_fitcecoc.m
+++ main_fitcecoc.m
@@ -49,7 +49,6 @@ montage(Eigenfaces(1:16));
 title('Top 16 Eigenfaces');
 colormap(gray);
 
-
 % NOTE: Rows of V are observations, columns are features.
 % Observations need to be in rows.
 k = min(size(V,2),k);
