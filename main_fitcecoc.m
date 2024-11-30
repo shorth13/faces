f6850c72d77b5a7e68b29cf9b2701129ac7e14b1
Marek Rychlik <rychlik@arizona.edu>
2024-11-30 08:02:07 -0700

    *** empty log message ***

--- main_fitcecoc.m
+++ main_fitcecoc.m
@@ -10,7 +10,7 @@
 % Binary classification
 % Distinguish between two persons (Angenlina Jolie and Eduardo Duhalde).
 targetSize=[128,128];
-k=70;                                   % Number of features to consider
+k=76;                                   % Number of features to consider
 location = fullfile('lfw');
 
 disp('Creating image datastore...');
