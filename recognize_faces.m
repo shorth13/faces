26bb8cdd24d20fdddbf64ffd270ee2f4b4d4de7c
Marek Rychlik <rychlik@arizona.edu>
2024-12-08 12:25:16 -0700

    *** empty log message ***

--- recognize_faces.m
+++ recognize_faces.m
@@ -1,5 +1,9 @@
-function recognize_faces(I)
+function YPred = recognize_faces(F)
+% recognize_faces - map images of faces to people names    
     load('model');
-    
+    % Extract features
+    X = V' * F;
+    % Predict faces
+    YPred = predict(Mdl, X)
 end
 
