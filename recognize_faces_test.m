bb6b09e3c40dc31afc7a3e539aefef199f55e374
Marek Rychlik <rychlik@arizona.edu>
2024-12-08 12:47:24 -0700

    *** empty log message ***

--- recognize_faces_test.m
+++ recognize_faces_test.m
@@ -1,6 +1,7 @@
 location = fullfile('lfw');
 
 disp('Creating image datastore...');
+% We read images in their original format
 imds = imageDatastore(location,...
                       'IncludeSubfolders',true,...
                       'LabelSource','foldernames');
