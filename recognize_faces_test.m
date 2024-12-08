3d5e69f29f8510bcf7ad6b68a8e18c293b882b01
Marek Rychlik <rychlik@arizona.edu>
2024-12-08 12:46:21 -0700

    *** empty log message ***

--- recognize_faces_test.m
+++ recognize_faces_test.m
@@ -3,4 +3,4 @@ location = fullfile('lfw');
 disp('Creating image datastore...');
 imds = imageDatastore(location,'IncludeSubfolders',true,'LabelSource','foldernames');
 
-
+load('model',["persons"])
