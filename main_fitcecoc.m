de6874f2200864e5d5e4271385fddd98d2a195e1
Marek Rychlik <rychlik@arizona.edu>
2024-12-09 18:16:39 -0700

    *** empty log message ***

--- main_fitcecoc.m
+++ main_fitcecoc.m
@@ -131,4 +131,4 @@ title(['Number of features: ' ,num2str(k)]);
 
 % Save the model and persons that the model recognizes.
 % NOTE: An important part of the submission.
-save('model','Mdl','persons','U','C','SD','targetSize');
+save('model','Mdl','persons','U', 'targetSize');
